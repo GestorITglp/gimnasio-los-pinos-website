39cc92ac7caa8e32f0180dfe4976cdb00399191f
Dacaramo <dankramirez@outlook.com>
2024-02-26 19:47:12 -0500

    feat: Added section 4 and 5 to IB page

--- src/app/[locale]/international-baccalaureate/page.tsx
+++ src/app/[locale]/international-baccalaureate/page.tsx
@@ -19,6 +19,11 @@ const InternationalBaccalaureatePage: FC<Props> = ({}) => {
   const pClasses = 'text-md sm:text-xl text-center z-[10]';
   const sectionClasses =
     'relative w-full px-[20px] lg:px-[200px] flex flex-col justify-center items-center gap-10 min-h-[50vh] bg-cover bg-no-repeat';
+  const normalBtnClasses =
+    'w-full sm:w-fit p-4 btn btn-lg btn-square text-white bg-dark-green-100 hover:bg-dark-green-200 border-0';
+  const blackishLayerClasses =
+    'absolute z-[0] left-0 top-0 w-full h-full bg-black opacity-25';
+
   return (
     <main className='flex flex-col gap-[50px]'>
       <section
@@ -101,6 +106,86 @@ const InternationalBaccalaureatePage: FC<Props> = ({}) => {
             })}
           </figcaption>
         </figure>
+      </section>
+      <section
+        id='section-4'
+        className='px-[200px] flex flex-col items-center gap-10'
+      >
+        <ul className='flex flex-row gap-10 justify-center items-center flex-wrap'>
+          <li className='relative w-[280px] h-[280px] bg-red-500 flex-1 rounded-lg flex justify-center items-center text-center p-4 text-lg font-bold text-white basis-[280px] min-w-[280px] max-w-[280px] bg-[url("/carrousel1.jpg")] bg-cover bg-center'>
+            <div
+              className={`absolute top-0 left-0 w-full h-full bg-dark-green-200 opacity-80 rounded-lg`}
+            />
+            <span className='z-[10]'>
+              {t('section-4.list.list-item-1-text')}
+            </span>
+          </li>
+          <li className='relative w-[280px] h-[280px] bg-red-500 flex-1 rounded-lg flex justify-center items-center text-center p-4 text-lg font-bold text-white basis-[280px] min-w-[280px] max-w-[280px] bg-[url("/carrousel2.jpg")] bg-cover bg-center'>
+            <div
+              className={`absolute top-0 left-0 w-full h-full bg-dark-green-200 opacity-80 rounded-lg`}
+            />
+            <span className='z-[10]'>
+              {t('section-4.list.list-item-2-text')}
+            </span>
+          </li>
+          <li className='relative w-[280px] h-[280px] bg-red-500 flex-1 rounded-lg flex justify-center items-center text-center p-4 text-lg font-bold text-white basis-[280px] min-w-[280px] max-w-[280px] bg-[url("/carrousel3.jpg")] bg-cover bg-center'>
+            <div
+              className={`absolute top-0 left-0 w-full h-full bg-dark-green-200 opacity-80 rounded-lg`}
+            />
+            <span className='z-[10]'>
+              {t('section-4.list.list-item-3-text')}
+            </span>
+          </li>
+          <li className='relative w-[280px] h-[280px] bg-red-500 flex-1 rounded-lg flex justify-center items-center text-center p-4 text-lg font-bold text-white basis-[280px] min-w-[280px] max-w-[280px] bg-[url("/carrousel5.jpg")] bg-cover bg-center'>
+            <div
+              className={`absolute top-0 left-0 w-full h-full bg-dark-green-200 opacity-80 rounded-lg`}
+            />
+            <span className='z-[10]'>
+              {t('section-4.list.list-item-4-text')}
+            </span>
+          </li>
+        </ul>
+        <Link
+          href='https://www.ibo.org/es/'
+          target='_blank'
+          type='button'
+          className={`${normalBtnClasses}`}
+        >
+          {t('section-4.button-text')}
+        </Link>
+      </section>
+      <section
+        id='section-5'
+        className='relative w-full min-h-[50vh] px-[20px] lg:px-[200px] py-[50px] flex flex-col gap-4 justify-center items-center bg-no-repeat bg-[url("/modelo.jpg")] bg-center sm:bg-[position:-200%_40%] bg-cover'
+      >
+        <div className={`${blackishLayerClasses} opacity-25`} />
+        <h2 className='text-2xl sm:text-[50px] z-[10] font-bold text-white leading-none text-center sm:text-start'>
+          {t('section-3.title')}
+        </h2>
+        <p className={`${pClasses} text-white z-[100]`}>
+          {t('section-3.description')}
+        </p>
+        <Link
+          className='w-full sm:w-fit p-4 btn btn-lg btn-square text-white bg-transparent border-2 border-white hover:bg-white hover:text-black hover:border-white z-[1000]'
+          target='_blank'
+          href='https://raw.githubusercontent.com/Dacaramo/ramzeis-quotes/master/Imagen%20Programa%20Escuela%20Primaria%20%E2%80%93%20PEP.png?token=GHSAT0AAAAAACMZL7CG2XQMQ4MXCNGWOI2GZOU5ILQ'
+        >
+          {t('section-5.pep-button-text')}
+        </Link>
+        <Link
+          className='w-full sm:w-fit p-4 btn btn-lg btn-square text-white bg-transparent border-2 border-white hover:bg-white hover:text-black hover:border-white z-[1000]'
+          target='_blank'
+          href='https://raw.githubusercontent.com/Dacaramo/ramzeis-quotes/master/Imagen%20Programa%20A%C3%B1os%20Intermedios%20%E2%80%93%20PAI.jpg?token=GHSAT0AAAAAACMZL7CGJP5FWEA6QWPLBR7QZOU5JZA'
+        >
+          {t('section-5.pai-button-text')}
+        </Link>
+        <Link
+          className='w-full sm:w-fit p-4 btn btn-lg btn-square text-white bg-transparent border-2 border-white hover:bg-white hover:text-black hover:border-white z-[1000]'
+          target='_blank'
+          href='https://raw.githubusercontent.com/Dacaramo/ramzeis-quotes/master/Imagen%20Programa%20Diploma%20-%20PD.jpg?token=GHSAT0AAAAAACMZL7CGPL74SFJBZERRSY6AZOU5LBA'
+        >
+          {t('section-5.pd-button-text')}
+        </Link>
       </section>
     </main>
   );
