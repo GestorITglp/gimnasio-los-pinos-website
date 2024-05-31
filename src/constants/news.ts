bbd253a031b4312134cf3b2946b2e9c5e68632bf
Ramondarioglp <151581560+Ramondarioglp@users.noreply.github.com>
2024-05-31 10:44:29 -0500

    Update news.ts

--- src/constants/news.ts
+++ src/constants/news.ts
@@ -10,7 +10,7 @@ export const noticias = [
     habilitada: true,
     titulo: '¡CUMPLIMOS 57 AÑOS!',
     descripcion:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vitae quam nec lacus molestie feugiat sed vitae magna. Aliquam pretium interdum diam, in tincidunt sapien porttitor at. Donec sit amet.',
+      '¡Felicidades al colegio Gimnasio Los Pinos por sus 57 años! Es un logro admirable llegar a esa marca, y estoy seguro de que es el resultado del arduo trabajo y dedicación de toda la comunidad educativa. Que sigan cosechando éxitos y contribuyendo al desarrollo integral de las futuras generaciones.',
     imagen:
       'https://i.pinimg.com/736x/1e/1c/0d/1e1c0df9a3224da6870b1dc96ebaead6.jpg',
   },
