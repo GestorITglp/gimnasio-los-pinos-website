494614aaa9bb1951a8f99b198bbd3e99793ac2d9
Ramondarioglp <151581560+Ramondarioglp@users.noreply.github.com>
2024-05-31 10:39:21 -0500

    Update news.ts

--- src/constants/news.ts
+++ src/constants/news.ts
@@ -3,16 +3,16 @@ export const noticias = [
     habilitada: false,
     titulo: 'Titulo de la noticia 1',
     descripcion:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vitae quam nec lacus molestie feugiat sed vitae magna. Aliquam pretium interdum diam, in tincidunt sapien porttitor at. Donec sit amet.[SALTO]Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vitae quam nec lacus molestie feugiat sed vitae magna. Aliquam pretium interdum diam, in tincidunt sapien porttitor at. Donec sit amet.[SALTO]Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vitae quam nec lacus molestie feugiat sed vitae magna. Aliquam pretium interdum diam, in tincidunt sapien porttitor at. Donec sit amet.[SALTO]Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vitae quam nec lacus molestie feugiat sed vitae magna. Aliquam pretium interdum diam, in tincidunt sapien porttitor at. Donec sit amet.[SALTO]Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vitae quam nec lacus molestie feugiat sed vitae magna. Aliquam pretium interdum diam, in tincidunt sapien porttitor at. Donec sit amet.[SALTO]Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vitae quam nec lacus molestie feugiat sed vitae magna. Aliquam pretium interdum diam, in tincidunt sapien porttitor at. Donec sit amet.',
+      '¡Felicidades al colegio Gimnasio Los Pinos por sus 57 años! Es un logro admirable llegar a esa marca, y estoy seguro de que es el resultado del arduo trabajo y dedicación de toda la comunidad educativa. Que sigan cosechando éxitos y contribuyendo al desarrollo integral de las futuras generaciones.',
     imagen: 'https://losmejorescolegios.com/wp-content/uploads/2023/10/13.png',
   },
   {
     habilitada: true,
-    titulo: 'Titulo de la noticia 2',
+    titulo: '¡CUMPLIMOS 57 AÑOS!',
     descripcion:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vitae quam nec lacus molestie feugiat sed vitae magna. Aliquam pretium interdum diam, in tincidunt sapien porttitor at. Donec sit amet.',
     imagen:
-      'https://www.gimnasiosanangelo.edu.co/wp-content/uploads/2022/09/Delegado-sobresaliente-y-Mejores-consiliadores-GLPMUN.webp',
+      'https://i.pinimg.com/736x/1e/1c/0d/1e1c0df9a3224da6870b1dc96ebaead6.jpg',
   },
   {
     habilitada: false,
