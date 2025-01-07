5e867a2a0ab0e72f3552f322791f9f1571229332
Ramondarioglp <151581560+Ramondarioglp@users.noreply.github.com>
2025-01-07 16:35:07 -0500

    Update links.ts

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -2,8 +2,8 @@
  * Recuerda que si el link que vas a añadir es para descargar un PDF o archivo similar, entonces debes poner ese archivo en la carpeta llamada "public". Una vez hecho esto, si el nombre del archivo cambió, entonces cambialo abajo también. EJEMPLO: Si el pdf del link linkMenuRestaurante cambió de llamarse menu1.pdf a llamarse menu2.pdf entonces el link debe cambiar de "/menu1.pdf" a "/menu2.pdf" no olvidar nunca poner el slash al comienzo del link.
  */
 export const linkClassroom =
-  'https://webapps-classroomliveweb.com:2443/pinos/2024/Login.aspx';
-export const linkMatricula = 'https://webapps-classroomliveweb.com:2443/pinos/2025/Login.aspx';
+  'https://webapps2-classroomliveweb.com:2443/pinos/2025/Login.aspx';
+export const linkMatricula = 'https://webapps2-classroomliveweb.com:2443/pinos/2025/Inscripcionprcita2.aspx';
 export const linkCalendario = '/CALENDARIO ACADEMICO.pdf';
 export const linkExtracurriculares = '/extracurriculares.pdf';
 export const linkManualConvivencia = '/convivencia.pdf';
